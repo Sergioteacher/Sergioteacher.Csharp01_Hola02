4466cd5f535c5c96144b8d14fb154c33f3890c54
Sergio Teacher <Sergio Teacher@MINIWIN>
2025-02-07 19:22:54 +0100

    Agregar archivos de proyecto.

---
+++ Sergioteacher.Csharp01_Hola02/HolaMundo02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Truco para acortar instrucciones.
+using output = System.Console;
+
+namespace Sergioteacher.Csharp01_Hola02
+{
+    class HolaMundo02
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Clear();
+            /*
+            Console.WriteLine("");
+            Console.WriteLine("¡Hola a todo el mundo --> 02!");
+            */
+            output.WriteLine("");
+            output.WriteLine("¡Hola a todo el mundo --> 02!");
+        }
+    }
+}
